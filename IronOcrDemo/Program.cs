a35e8d6b5adbb2cd57f21ad4323050cf4f4c6d04
Mujeeb Ayobami Lawal <ayolawal247@outlook.com>
2023-06-07 16:33:04 +0100

    Add project files.

---
+++ IronOcrDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using IronOcr;
+using System;
+
+namespace OcrTryout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ocr = new IronTesseract();
+
+            // Load image
+            var image = "img_path.jpg";
+
+            // Perform OCR
+            var result = ocr.Read(image);
+
+            // Access extracted text
+            string extractedText = result.Text;
+
+            // Display the extracted text
+            Console.WriteLine(extractedText);
+
+            // Wait for the user input to exit
+            Console.ReadLine();
+        }
+    }
+}
